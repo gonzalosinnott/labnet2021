23e24f2582d84c1ac40d8c3991e3901e878b1683
Gonzalo Sinnott Segura <gonzalo.sinnott@gmail.com>
2021-09-09 18:18:58 -0300

    CAMBIO LABELS RADIO BUTTONS
    Cambio de los labels de los radio buttons porque sino no agregaba a la lista de objetos.

---
+++ LabNet2021.TP01/Entities/Clases/TransportePublico.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public abstract class TransportePublico
+    {
+        protected int pasajeros;
+        protected int id;
+
+        public TransportePublico() { }
+
+        protected TransportePublico(int pasajeros, int id)
+        {
+            this.pasajeros = Pasajeros;
+            this.id = Id;
+        }
+        public virtual int Pasajeros { get; set; }
+
+        public virtual int Id { get; set; }
+
+        public abstract void Avanzar();
+
+        public abstract void Detenerse();
+
+        public string getClassType()
+        {
+            return this.GetType().Name;
+        }
+
+        public string ShowVehicleData()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{this.getClassType()} Numero: {this.Id} - Cantidad de Pasajeros: {this.Pasajeros}");
+
+            return sb.ToString();
+        }
+    }
+}
