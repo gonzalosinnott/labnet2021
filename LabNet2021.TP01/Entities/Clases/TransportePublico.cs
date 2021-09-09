4a7af13144b9ffcbef58027db375ce0446e2f7d4
Gonzalo Sinnott Segura <gonzalo.sinnott@gmail.com>
2021-09-09 12:20:49 -0300

    CASCARA
    Modelado de la cascara del proyecto

---
+++ LabNet2021.TP01/Entities/Clases/TransportePublico.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public abstract class TransportePublico
+    {
+        private int pasajeros;
+        private int id;
+
+        public TransportePublico(int pasajeros, int id)
+        {
+            this.pasajeros = pasajeros;
+            this.id = id;
+        }
+        public int Pasajeros { get; set; }
+
+        public int Id { get; set; }
+
+        public abstract void Avanzar();
+
+        public abstract void Detenerse();
+    }
+}
