b61c880159845133e558daffb378515ad79a9787
Gonzalo Sinnott Segura <gonzalo.sinnott@gmail.com>
2021-09-18 04:38:33 -0300

    CAMBIOS ESTETICOS

--- LabNet2021.TP04/LabNet2021.TP04.GUI/frmMain.Designer.cs
+++ LabNet2021.TP04/LabNet2021.TP04.GUI/frmMain.Designer.cs
@@ -71,7 +71,7 @@ namespace LabNet2021.TP04.GUI
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage1.Size = new System.Drawing.Size(1063, 345);
             this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "Customers";
+            this.tabPage1.Text = "CLIENTES";
             this.tabPage1.UseVisualStyleBackColor = true;
             // 
             // btnModifyCostumer
@@ -154,7 +154,7 @@ namespace LabNet2021.TP04.GUI
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage2.Size = new System.Drawing.Size(1063, 345);
             this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "Employees";
+            this.tabPage2.Text = "EMPLEADOS";
             this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // dgvEmployees
