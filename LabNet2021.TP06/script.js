4170740bb59ff0e0d3492fdc8243f4ef00716b07
Gonzalo Sinnott Segura <gonzalo.sinnott@gmail.com>
2021-09-25 15:10:08 -0300

    Agregado Estilo y Funcionalidad

--- LabNet2021.TP06/script.js
+++ LabNet2021.TP06/script.js
@@ -1,1 +1,26 @@
 "use strict";
+
+$(document).ready(function()
+{
+    $('#submit').click(function()
+    {
+        if (!$('#txtNombre').val())
+        {
+            alert('INGRESE SU NOMBRE');
+        }
+        else if (!$('#txtApellido').val())
+        {
+            alert('INGRESE SU APELLIDO');
+        }      
+    })
+
+    $("#clean").click(function()
+    {
+        $('#form').find('input:text').val('');
+        $("input[type=number]").val('');
+        if($("input:radio[name=sexo]").is(':checked')) 
+        {
+            $("input:radio[name=sexo]:checked")[0].checked = false;
+        }
+    }) 
+});
