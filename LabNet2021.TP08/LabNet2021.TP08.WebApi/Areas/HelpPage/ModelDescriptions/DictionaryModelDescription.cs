8ffee1630e95525aa004d951fa4d98c6d05289c5
Gonzalo Sinnott Segura <gonzalo.sinnott@gmail.com>
2021-10-03 20:31:01 -0300

    Metodo Get de Tabla Shippers

---
+++ LabNet2021.TP08/LabNet2021.TP08.WebApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace LabNet2021.TP08.WebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ LabNet2021.TP08/LabNet2021.TP08.WebApi/Controllers/ShippersController.cs
@@ -0,0 +1,38 @@
+﻿using LabNet2021.TP04.Data;
+using LabNet2021.TP04.Entities;
+using LabNet2021.TP04.Logic;
+using LabNet2021.TP04.Logic.Clases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LabNet2021.TP08.WebApi.Controllers
+{
+    public class ShippersController : ApiController
+    {
+        public IHttpActionResult GetAllShippers()
+        {
+            IList<ShippersDTO> shippers = null;
+
+            using (var context = new NorthwindContext())
+            {
+                shippers = context.Shippers.Select(s => new ShippersDTO()
+                            {
+                                ShipperID = s.ShipperID,
+                                CompanyName = s.CompanyName,
+                                Phone = s.Phone
+                            }).ToList();
+            }
+
+            if (shippers.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(shippers);
+        }
+    }
+}
